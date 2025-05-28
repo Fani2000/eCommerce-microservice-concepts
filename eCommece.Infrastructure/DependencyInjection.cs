30c81eb981d0f6cf61ebf61ec93f50af38d663be
Fani Keorapetse <keorapetse.fani@dimensiondata.com>
2025-05-28 18:21:39 +0200

    Updated the User, Middlware, and also the default setup for the user

---
+++ eCommece.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommece.Infrastructure;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Extension method to add infrastructure services to the dependency injection container
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+       // TODO: Add Service to the IoC container 
+       
+       return services; 
+    }
+}
---
+++ eCommerce.API/Program.cs
@@ -0,0 +1,25 @@
+using eCommece.Infrastructure;
+using eCommerce.API.Middlewares;
+using eCommerce.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Infrastructure services
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+// Add Controllers to the service collection
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
